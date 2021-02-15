bd98a59f691fb9aee0b9b313baed79792ff655ac
Takaaki Umedu <ta-umedu@biwako.shiga-u.ac.jp>
2021-02-16 01:37:49 +0900

    implementation of product completed

--- range.hpp
+++ range.hpp
@@ -23,45 +23,50 @@ namespace iterable{
 		inline T end() const{ return _e; }
 	};
 	
-	class auto_t{ private: auto_t(); };
+	class auto_t    final{ private: auto_t()    = delete; };
+	class flatten_t final{ private: flatten_t() = delete; };
 	
 	namespace {
 		template<typename E> using iter_b = decltype(declval<E>().begin());
 		template<typename E> using iter_e = decltype(declval<E>().end());
 		
 		template<typename P, typename I> inline auto make_tuple_from_iter(I i){
-			using A                  = decltype(*declval<I>());
-			using A_non_ref          = typename remove_reference<A>::type;
-			using A_naked            = typename remove_const<A_non_ref>::type;
-			const bool A_const       = is_const<A_non_ref>::value;
-			const bool A_ref         = is_lvalue_reference<A>::value;
-			const bool A_fundamental = is_fundamental<A_naked>::value;
-			
-			using P_non_ref          = typename remove_reference<P>::type;
-			using P_naked_tmp        = typename remove_const<P_non_ref>::type;
-			const bool P_auto        = is_same<P_naked_tmp, auto_t>::value;
-			const bool P_const_tmp   = is_const<P_non_ref>::value;
-			const bool P_ref_tmp     = is_lvalue_reference<P>::value;
-			const bool P_pure_auto   = P_auto && !P_const_tmp && !P_ref_tmp;
-			
-			using P_naked            = conditional_t<P_auto, A_naked, P_naked_tmp>;
-			const bool P_const       = (P_const_tmp || (P_pure_auto && !A_fundamental && A_ref));
-			const bool P_ref         = (P_ref_tmp   || (P_pure_auto && !A_fundamental && A_ref));
-			
-			static_assert(!(P_ref && !P_const && A_const), "cannot make non const reference to const lvalue");
-			static_assert(!(P_ref && !A_ref), "cannot make reference for rvalue");
-			
-			if constexpr(P_ref){
-				if constexpr(P_const){
-					return (tuple<const P_naked&>)make_tuple(cref(*i));
-				}else{
-					return (tuple<P_naked&>)make_tuple(ref(*i));
-				}
+			if constexpr(is_same_v<P, flatten_t>){
+				return *i;
 			}else{
-				if constexpr(P_const){
-					return (tuple<const P_naked>)make_tuple(*i);
+				using A                  = decltype(*declval<I>());
+				using A_non_ref          = remove_reference_t<A>;
+				using A_naked            = remove_const_t<A_non_ref>;
+				const bool A_const       = is_const_v<A_non_ref>;
+				const bool A_ref         = is_lvalue_reference_v<A>;
+				const bool A_fundamental = is_fundamental_v<A_naked>;
+				
+				using P_non_ref          = remove_reference_t<P>;
+				using P_naked_tmp        = remove_const_t<P_non_ref>;
+				const bool P_auto        = is_same_v<P_naked_tmp, auto_t>;
+				const bool P_const_tmp   = is_const_v<P_non_ref>;
+				const bool P_ref_tmp     = is_lvalue_reference_v<P>;
+				const bool P_pure_auto   = P_auto && !P_const_tmp && !P_ref_tmp;
+				
+				using P_naked            = conditional_t<P_auto, A_naked, P_naked_tmp>;
+				const bool P_const       = (P_const_tmp || (P_pure_auto && !A_fundamental && A_ref));
+				const bool P_ref         = (P_ref_tmp   || (P_pure_auto && !A_fundamental && A_ref));
+				
+				static_assert(!(P_ref && !P_const && A_const), "cannot make non const reference to const lvalue");
+				static_assert(!(P_ref && !A_ref), "cannot make reference for rvalue");
+				
+				if constexpr(P_ref){
+					if constexpr(P_const){
+						return (tuple<const P_naked&>)make_tuple(cref(*i));
+					}else{
+						return (tuple<P_naked&>)make_tuple(ref(*i));
+					}
 				}else{
-					return (tuple<P_naked>)make_tuple(*i);
+					if constexpr(P_const){
+						return (tuple<const P_naked>)make_tuple(*i);
+					}else{
+						return (tuple<P_naked>)make_tuple(*i);
+					}
 				}
 			}
 		}
@@ -110,125 +115,122 @@ namespace iterable{
 		};
 		
 		template<typename T, int index, typename C, typename... Cs> inline auto make_zip_container(C&& c, Cs&&... cs){
-			using container_type = conditional_t<is_lvalue_reference<C>::value, C&, C>;
+			using container_type = conditional_t<is_lvalue_reference_v<C>, C&, C>;
 			if constexpr(sizeof...(Cs) == 0){
-				return zip_container<typename tuple_element<index, T>::type, container_type>(forward<C>(c));
+				return zip_container<tuple_element_t<index, T>, container_type>(forward<C>(c));
 			}else{
-				return zip_container<typename tuple_element<index, T>::type, container_type, decltype(make_zip_container<T, index + 1, Cs...>(forward<Cs>(cs)...))>(forward<C>(c), make_zip_container<T, index + 1, Cs...>(forward<Cs>(cs)...));
+				return zip_container<tuple_element_t<index, T>, container_type, decltype(make_zip_container<T, index + 1, Cs...>(forward<Cs>(cs)...))>(forward<C>(c), make_zip_container<T, index + 1, Cs...>(forward<Cs>(cs)...));
 			}
 		}
-
+		
 		template<size_t N, typename T, typename... Ts> class tuple_type_builder             { public: using type = typename tuple_type_builder<N - 1, T, T, Ts...>::type; };
 		template<          typename T, typename... Ts> class tuple_type_builder<0, T, Ts...>{ public: using type = tuple<Ts...>; };
 		
-		template<typename T, typename... Cs> using make_tuple_type_from_arguments = conditional_t<
-			is_same<typename remove_const<typename remove_reference<T>::type>::type, auto_t>::value,
-			typename tuple_type_builder<sizeof...(Cs), T>::type,
+		template<size_t N, typename T> using make_tuple_type_from_arguments = conditional_t<
+			is_same_v<remove_const_t<remove_reference_t<T>>, auto_t>,
+			typename tuple_type_builder<N, T>::type,
 			T
-		>;	
+		>;
+		
+		template<size_t N, size_t O, typename T1, typename T2> class tuple_element_checker{
+			public: static const bool value = tuple_element_checker<N - 1, O, T1, T2>::value && is_same_v<tuple_element_t<N + O, T1>, tuple_element_t<N, T2>>;
+		};
+		template<size_t O, typename T1, typename T2> class tuple_element_checker<0, O, T1, T2>{
+			public: static const bool value = is_same_v<tuple_element_t<O, T1>, tuple_element_t<0, T2>>;
+		};
+		template<typename T1, typename T2> using tuple_check_head = tuple_element_checker<tuple_size_v<T2> - 1, 0, T1, T2>;
+		template<typename T1, typename T2> using tuple_check_tail = tuple_element_checker<tuple_size_v<T2> - 1, tuple_size_v<T1> - tuple_size_v<T2>, T1, T2>;
 	}
 
 	template<typename T = auto_t, typename... Cs> inline auto zip(Cs&&... cs){
-		using tuple_type = make_tuple_type_from_arguments<T, Cs...>;
+		using tuple_type = make_tuple_type_from_arguments<sizeof...(Cs), T>;
 		
-		static_assert(tuple_size<tuple_type>::value == sizeof...(Cs), "the numbers of tuple elements of parameter and arguments are not matched");
+		static_assert(tuple_size_v<tuple_type> == sizeof...(Cs), "the numbers of tuple elements of parameter and arguments are not matched");
 		return make_zip_container<tuple_type, 0, Cs...>(forward<Cs>(cs)...);
 	}
 
 	template<typename T = auto_t, typename... Cs> inline auto enumerate(Cs&&... cs){
-		using tuple_type = decltype(tuple_cat(make_tuple<size_t>(0), declval<make_tuple_type_from_arguments<T, Cs...>>()));
+		using tuple_type = decltype(tuple_cat(make_tuple<size_t>(0), declval<make_tuple_type_from_arguments<sizeof...(Cs), T>>()));
 		
 		return zip<tuple_type>(range<size_t>(numeric_limits<size_t>::max()), forward<Cs>(cs)...);
 	}
-/*
-	template<typename T = tuple<auto_t>, typename... Cs> auto zip(Cs&&... cs){
-		using tuple_type = conditional_t<
-			is_same<T, tuple<auto_t>>::value,
-			tuple_type_from_containers<Cs...>,
-			T
-		>;
-		
-		static_assert(tuple_size<tuple_type>::value == sizeof...(Cs), "number of tuple template parameter and arguments are not matched");
-		return make_zip_container<tuple_type, 0, Cs...>(forward<Cs>(cs)...);
-	}
-
-	template<typename T = tuple<auto_t>, typename... Cs> auto enumerate(Cs&&... cs){
-		using tuple_type = conditional_t<
-			is_same<T, tuple<auto_t>>::value,
-			tuple_type_from_containers<int, Cs...>,
-			decltype(tuple_cat(make_tuple<int>(0), (T)declval<T>()))
-		>;
-		return zip<tuple_type>(range(numeric_limits<int>::max()), forward<Cs>(cs)...);
-	}
-*/	
-/*
-	enum { EE, EP, PE, PP };
 	
-	template<int K, typename E1, typename E2> class product_iter{
-	protected:
-		iter_b<E1> _i1i;
-		iter_e<E1> _i1e;
-		iter_b<E2> _i2b, _i2i;
-		iter_e<E2> _i2e;
-		bool not_ended = true;
-	public:
-		product_iter(iter_b<E1> i1b, iter_e<E1> i1e, iter_b<E2> i2b, iter_e<E2> i2e): _i1i(i1b), _i1e(i1e), _i2b(i2b), _i2i(_i2b), _i2e(i2e){}
-		inline void operator++(){
-			++_i2i;
-			if(!(_i2i != _i2e)){
-				_i2i = _i2b;
-				++_i1i;
-				if(!(_i1i != _i1e)){
-					not_ended = false;
+	namespace{
+		template<typename V1, typename V2, typename C1, typename C2> class product_iter{
+		protected:
+			iter_b<C1> _i1i;
+			iter_e<C1> _i1e;
+			iter_b<C2> _i2b, _i2i;
+			iter_e<C2> _i2e;
+			bool not_ended = true;
+		public:
+			inline product_iter(iter_b<C1> i1b, iter_e<C1> i1e, iter_b<C2> i2b, iter_e<C2> i2e): _i1i(i1b), _i1e(i1e), _i2b(i2b), _i2i(_i2b), _i2e(i2e){}
+			inline void operator++(){
+				++_i2i;
+				if(!(_i2i != _i2e)){
+					_i2i = _i2b;
+					++_i1i;
+					if(!(_i1i != _i1e)){
+						not_ended = false;
+					}
 				}
 			}
+			inline void operator++(int){ operator++(); }
+			template<typename T> inline bool operator!=(const T&) const{ return not_ended; }
+			inline auto operator*() const{ return tuple_cat(make_tuple_from_iter<V1>(this->_i1i), make_tuple_from_iter<V2>(this->_i2i)); }
+		};
+		
+		class product_container_base{};
+		
+		template<typename C1, typename C2, typename I> class product_container: product_container_base{
+			C1 _c1;
+			C2 _c2;
+		public:
+			inline product_container(C1 c1, C2 c2): _c1(c1), _c2(c2){}
+			inline auto begin() const{ return I(_c1.begin(), _c1.end(), _c2.begin(), _c2.end()); }
+			inline int end() const{ return 0; }
+		};
+		
+		template<typename T> class replace_tuple_head{};
+		template<typename C1, typename C2, typename... Cs> class replace_tuple_head<tuple<C1, C2, Cs...>>{ public: using type = tuple<flatten_t, Cs...>; };
+	}
+	
+	template<typename T = auto_t, typename C1, typename C2, typename... Cs> inline auto product(C1&& c1, C2&& c2, Cs&&... cs){
+		using tuple_type = make_tuple_type_from_arguments<2 + sizeof...(Cs), T>;
+		
+		static_assert(tuple_size_v<tuple_type> == 2 + sizeof...(Cs), "the number of elements in requested tuple type is not matched with arguments");
+		
+		const bool c1_is_product = is_base_of_v<product_container_base, C1>;
+		const bool c2_is_product = is_base_of_v<product_container_base, C2>;
+		
+		using type1_tmp = tuple_element_t<0, tuple_type>;
+		using type2_tmp = tuple_element_t<1, tuple_type>;
+		
+		static_assert(!c1_is_product || is_same_v<type1_tmp, auto_t> || is_same_v<type1_tmp, flatten_t>, "type request for argument 1 is invalid, auto_t or flatten_t is required");
+		static_assert(!c2_is_product || is_same_v<type2_tmp, auto_t> || is_same_v<type2_tmp, flatten_t>, "type request for argument 2 is invalid, auto_t or flatten_t is required");
+		
+		using type1 = conditional_t<conjunction_v<is_same<type1_tmp, auto_t>, bool_constant<c1_is_product>>, flatten_t, type1_tmp>;
+		using type2 = conditional_t<conjunction_v<is_same<type2_tmp, auto_t>, bool_constant<c2_is_product>>, flatten_t, type2_tmp>;
+		
+		using container_type1 = conditional_t<is_lvalue_reference_v<C1>, C1&, C1>;
+		using container_type2 = conditional_t<is_lvalue_reference_v<C2>, C2&, C2>;
+		
+		using iter_type = product_iter<type1, type2, container_type1, container_type2>;
+		
+		auto p = product_container<container_type1, container_type2, iter_type>(forward<C1>(c1), forward<C2>(c2));
+		
+		if constexpr(sizeof...(Cs) == 0){
+			return p;
+		}else{
+			return product<typename replace_tuple_head<tuple_type>::type>(move(p), forward<Cs>(cs)...);
 		}
-		inline void operator++(int){ operator++(); }
-		template<typename T> inline bool operator!=(const T&) const{ return not_ended; }
-		inline auto operator*() const{
-			if constexpr(K == EE) return make_tuple_from_iters(this->_i1i, this->_i2i);
-			if constexpr(K == EP) return tuple_cat(make_tuple_from_iter(this->_i1i), *this->_i2i);
-			if constexpr(K == PE) return tuple_cat(*this->_i1i, make_tuple_from_iter(this->_i2i));
-			if constexpr(K == PP) return tuple_cat(*this->_i1i, *this->_i2i);
-		}
-	};
-
-	template<typename E1, typename E2, typename I> class product_type{
-		E1 _e1;
-		E2 _e2;
-	public:
-		product_type(E1 e1, E2 e2): _e1(e1), _e2(e2){}
-		inline auto begin() const{ return I(_e1.begin(), _e1.end(), _e2.begin(), _e2.end()); }
-		inline int end() const{ return 0; }
-	};
+	}
 	
-	template<typename E1, typename E2, typename E3, typename I> auto product(const product_type<E1, E2, I>&& p12,       E3&  e3){ return product_type<product_type<E1, E2, I>, E3&, product_iter<PE, product_type<E1, E2, I>, E3>>(p12, e3); }
-	template<typename E1, typename E2, typename E3, typename I> auto product(const product_type<E1, E2, I>&& p12, const E3&& e3){ return product_type<product_type<E1, E2, I>, E3 , product_iter<PE, product_type<E1, E2, I>, E3>>(p12, e3); }
-	template<typename E1, typename E2, typename E3, typename I> auto product(      E1&  e1, const product_type<E2, E3, I>&& p23){ return product_type<E1&, product_type<E2, E3, I>, product_iter<EP, E1, product_type<E2, E3, I>>>(e1, p23); }
-	template<typename E1, typename E2, typename E3, typename I> auto product(const E1&& e1, const product_type<E2, E3, I>&& p23){ return product_type<E1 , product_type<E2, E3, I>, product_iter<EP, E1, product_type<E2, E3, I>>>(e1, p23); }
-	template<typename E1, typename E2, typename E3, typename E4, typename I12, typename I34> auto product(const product_type<E1, E2, I12>&& p12, const product_type<E3, E4, I34>&& p34){ return product_type<product_type<E1, E2, I12>, product_type<E3, E4, I34>, product_iter<PP, product_type<E1, E2, I12>, product_type<E3, E4, I34>>>(p12, p34); }
-
-	//the following does not work. I don't know why...
-//	template<typename E1, typename E2, typename E3, typename I, typename P12 = product_type<E1, E2, I>> auto product(const P12&& p12,       E3&  e3){ return product_type<P12, E3&, product_iter_pe<P12, E3>>(p12, e3); }
-//	template<typename E1, typename E2, typename E3, typename I, typename P12 = product_type<E1, E2, I>> auto product(const P12&& p12, const E3&& e3){ return product_type<P12, E3 , product_iter_pe<P12, E3>>(p12, e3); }
-//	template<typename E1, typename E2, typename E3, typename I, typename P23 = product_type<E2, E3, I>> auto product(      E1&  e1, const P23&& p23){ return product_type<E1&, P23, product_iter_ep<E1, P23>>(e1, p23); }
-//	template<typename E1, typename E2, typename E3, typename I, typename P23 = product_type<E2, E3, I>> auto product(const E1&& e1, const P23&& p23){ return product_type<E1 , P23, product_iter_ep<E1, P23>>(e1, p23); }
-//	template<typename E1, typename E2, typename E3, typename E4, typename I12, typename I34, typename P12 = product_type<E1, E2, I12>, typename P34 = product_type<E3, E4, I34>> auto product(const P12&& p12, const P34&& p34){ return product_type<P12, P34, product_iter_pp<P12, P34>>(p12, p34); }
-
+	template<typename C1, typename C2, typename I1B = iter_b<C1>, typename I1E = iter_e<C1>, typename I2B = iter_b<C2>, typename I2E = iter_e<C2>> inline auto operator*(C1&& c1, C2&& c2){ return product(forward<C1>(c1), forward<C2>(c2)); }
 	
-	template<typename E1, typename E2> inline auto product(E1&        e1, E2&        e2){ return product_type<E1&, E2&, product_iter<EE, E1, E2>>(     e1,       e2);  }
-	template<typename E1, typename E2> inline auto product(E1&        e1, const E2&& e2){ return product_type<E1&, E2 , product_iter<EE, E1, E2>>(     e1,  move(e2)); }
-	template<typename E1, typename E2> inline auto product(const E1&& e1, E2&        e2){ return product_type<E1 , E2&, product_iter<EE, E1, E2>>(move(e1),      e2);  }
-	template<typename E1, typename E2> inline auto product(const E1&& e1, const E2&& e2){ return product_type<E1 , E2 , product_iter<EE, E1, E2>>(move(e1), move(e2)); }
-	
-	template<typename E1, typename E2, typename I1B = iter_b<E1>, typename I1E = iter_e<E1>, typename I2B = iter_b<E2>, typename I2E = iter_e<E2>> inline auto operator*(E1& e1, E2& e2){ return product(e1, e2); }
-	template<typename E1, typename E2, typename I1B = iter_b<E1>, typename I1E = iter_e<E1>, typename I2B = iter_b<E2>, typename I2E = iter_e<E2>> inline auto operator*(E1& e1, const E2&& e2){ return product(e1, move(e2)); }
-	template<typename E1, typename E2, typename I1B = iter_b<E1>, typename I1E = iter_e<E1>, typename I2B = iter_b<E2>, typename I2E = iter_e<E2>> inline auto operator*(const E1&& e1, E2& e2){ return product(move(e1), e2); }
-	template<typename E1, typename E2, typename I1B = iter_b<E1>, typename I1E = iter_e<E1>, typename I2B = iter_b<E2>, typename I2E = iter_e<E2>> inline auto operator*(const E1&& e1, const E2&& e2){ return product(move(e1), move(e2)); }
-*/
-//	inline auto mdrange(int n){ return range(n); }
-//	inline auto mdrange(const range&& r){ return r; }
-//	template<typename ...Rs> inline auto mdrange(int n, Rs... rest){ return range(n) * mdrange(forward<Rs>(rest)...); }
-//	template<typename ...Rs> inline auto mdrange(const range&& r, Rs... rest){ return r * mdrange(forward<Rs>(rest)...); }
+	template<typename T = size_t> inline auto mdrange(int n){ return range<T>(n); }
+	template<typename T>          inline auto mdrange(range<T>&& r){ return r; }
+	template<typename T = size_t, typename ...Rs> inline auto mdrange(int n, Rs&&... rest){ return forward<range<T>>(range<T>(n)) * mdrange(forward<Rs>(rest)...); }
+	template<typename T, typename ...Rs>          inline auto mdrange(range<T>&& r, Rs&&... rest){ return forward<range<T>>(r) * mdrange(forward<Rs>(rest)...); }
 }
 
